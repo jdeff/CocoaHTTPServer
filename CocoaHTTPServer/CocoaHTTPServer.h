71337a25fd29a0ba18ba993b608548d9d53daa48
jdeff <jdeff@google.com>
2017-06-20 10:28:36 -0600

    Re-organize project using Pods instead of Vendor dir

---
+++ CocoaHTTPServer/CocoaHTTPServer.h
@@ -0,0 +1,8 @@
+//! Project version number for CocoaHTTPServer.
+FOUNDATION_EXPORT double CocoaHTTPServerVersionNumber;
+
+//! Project version string for Foo.
+FOUNDATION_EXPORT const unsigned char CocoaHTTPServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Foo/PublicHeader.h>
+#import <CocoaHTTPServer/HTTPServer.h>
---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, 11
+
+target 'CocoaHTTPServer' do
+  project 'CocoaHTTPServer/CocoaHTTPServer.xcodeproj'
+
+  inhibit_all_warnings!
+
+  pod 'CocoaLumberjack'
+  pod 'CocoaAsyncSocket'
+end
